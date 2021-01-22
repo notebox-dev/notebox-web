f1e1e1c63fce0bb3c2072c19f84e979290dc18ee
yarastqt <yarastqt@gmail.com>
2021-01-23 01:06:32 +0300

    chore: init repo

---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Home.css'
+
+class Home extends React.Component {
+  render() {
+    return <div className="Home">home page</div>
+  }
+}
+
+export default Home
